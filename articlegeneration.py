a7bb4c2ebb5860d5299852de808d2d773952cf15
polina378 <36273979+polina378@users.noreply.github.com>
2023-09-08 01:28:20 -0400

    Update articlegeneration.py

--- articlegeneration.py
+++ articlegeneration.py
@@ -1,4 +1,3 @@
-import requests
 from langchain.llms import CTransformers
 from langchain.chains import LLMChain
 from langchain import PromptTemplate
@@ -8,35 +7,7 @@ from docx import Document
 from docx.shared import Inches
 import io
 from PIL import Image
-
-def fetch_photo(query):
-    api_key = '9ly611qdSF70tNVdPGZpZHPsscis7RCOsMmEg4fZFWTSs2mz3buhWZMk'
-
-    url = 'https://api.pexels.com/v1/search'
-    headers = {
-        'Authorization': api_key,
-    }
-
-    params = {
-        'query': query,
-        'per_page': 1,
-    }
-
-    response = requests.get(url, headers=headers, params=params)
-
-    # Check if the request was successful (status code 200)
-    if response.status_code == 200:
-        data = response.json()
-        photos = data.get('photos', [])
-        if photos:
-            src_original_url = photos[0]['src']['original']
-            return src_original_url
-        else:
-            print("No photos found for the given query.")
-    else:
-        print(f"Error: {response.status_code}, {response.text}")
-    
-    return None
+import requests
 
 #Loading the model
 def load_llm(max_tokens, prompt_template):
@@ -58,7 +29,7 @@ def load_llm(max_tokens, prompt_template):
 def get_src_original_url(query):
     url = 'https://api.pexels.com/v1/search'
     headers = {
-        'Authorization': '9ly611qdSF70tNVdPGZpZHPsscis7RCOsMmEg4fZFWTSs2mz3buhWZMk',
+        'Authorization': "9ly611qdSF70tNVdPGZpZHPsscis7RCOsMmEg4fZFWTSs2mz3buhWZMk",
     }
 
     params = {
